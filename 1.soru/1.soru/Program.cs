67b8f4c9c307c4588b67d1972862ba2f0ccf4a80
genishasan2 <39311599+genishasan2@users.noreply.github.com>
2018-05-31 00:19:22 +0300

    Study Case

---
+++ 1.soru/1.soru/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.soru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 10, 10 };
+            int[] B = { 30, 20 };
+            int[] C = { 20, 40 };
+            int[] D = { 20, 30 };
+            int bx, by, cx, cy, x, y;
+            double d,WA,WB,WC;
+            bx = B[0] - A[0]; by= B[1] - A[1];
+            cx = C[0] - A[0];cy= C[1] - A[1];
+            x = D[0] - A[0]; y = D[1] - A[1];
+            d = bx * cy - cx * by;
+            WA = (x * (by - cy) + y * (cx - bx) + bx * cy - cx * by)/d;
+            WB = (x * cy - y * cx) / d;
+            WC = (y * bx - x * by) / d;
+            if (0<WA && WA<1 && 0<WB && WB<1 && 0<WC && WC<1)
+            {
+                Console.WriteLine("Nokta üçgen alanının içerisinde");
+            }
+            else
+            {
+                Console.WriteLine("Nokta üçgen alanının içerisinde değil");
+            }
+            Console.ReadKey();
+        }
+    }
+}
